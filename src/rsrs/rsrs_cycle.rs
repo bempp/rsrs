04c26148abc82fd24310839ef059c39e7b131cb9
Ignacia Piccardo <ignacia-fp@Ignacias-MacBook-Air.local>
2025-05-30 09:06:12 +0100

    print fixed

--- src/rsrs/rsrs_cycle.rs
+++ src/rsrs/rsrs_cycle.rs
@@ -337,15 +337,17 @@ where
         println!("Extraction time: {:?}s\n", extraction_time.as_secs());
         self.stats.extraction_time = extraction_time.as_millis();
         let duration = algo_start.elapsed();
+        self.stats.total_elapsed_time = duration.as_millis();
+        let sampling_time = self.stats.sampling_extraction_time + self.stats.sampling_time.iter().sum::<u128>();
+        self.stats.total_elapsed_time_wo_sampling = self.stats.total_elapsed_time.saturating_sub(
+            sampling_time,
+        );
         println!(
-            "Total elapsed time: {:?}, with {} active samples\n",
-            duration, self.active_samples
+            "Total elapsed time: {:?} ({}ms for sampling, {}ms for RSRS), with {} active samples\n",
+            duration, sampling_time, self.stats.total_elapsed_time_wo_sampling, self.active_samples
         );
         println!("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n");
-        self.stats.total_elapsed_time = duration.as_millis();
-        self.stats.total_elapsed_time_wo_sampling = self.stats.total_elapsed_time.saturating_sub(
-            self.stats.sampling_extraction_time + self.stats.sampling_time.iter().sum::<u128>(),
-        );
+        
 
         rsrs_factors
     }
