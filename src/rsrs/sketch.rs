db8d87ba1cea6d7c8ed675d32c8c12af78905ad9
Ignacia Piccardo <ignacia-fp@Ignacias-MacBook-Air.local>
2025-06-28 07:54:50 +0100

    fix in sampling

--- src/rsrs/sketch.rs
+++ src/rsrs/sketch.rs
@@ -83,7 +83,7 @@ where
     Standard: Distribution<Item::Real>,
 {
     fn sampling<R: Rng>(&self, x: &mut Element<ConcreteElementContainer<Self::E>>, rng: &mut R) {
-        x.view_mut().fill_from_equally_distributed_real(rng);
+        x.view_mut().fill_from_normally_distributed_real(rng);
     }
 
     fn zero(space: std::rc::Rc<Self>) -> Element<ConcreteElementContainer<Self::E>> {
