7f8b6176c40fd59eecadecf82491c35f4f60fa68
Ignacia Piccardo <ignacia-fp@Ignacias-MacBook-Air.local>
2025-08-27 20:09:12 +0100

    fixed max rank

--- src/rsrs/rsrs_factors.rs
+++ src/rsrs/rsrs_factors.rs
@@ -994,7 +994,7 @@ where
                         )
                         .unwrap()
                 } else {
-                    let loc_rank = null_shape[0].min(num::ToPrimitive::to_usize(tol).unwrap());
+                    let loc_rank = null_shape[1].min(num::ToPrimitive::to_usize(tol).unwrap());
                     far_field_sketch
                         .into_subview([0, 0], null_shape)
                         .into_id_alloc(
