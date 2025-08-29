685cbe92d326573d914bff754518d107ba0bcf05
Ignacia Piccardo <ignacia-fp@Ignacias-MacBook-Air.local>
2025-08-29 12:34:45 +0100

    debug prints

--- src/rsrs/rsrs_factors.rs
+++ src/rsrs/rsrs_factors.rs
@@ -985,6 +985,7 @@ where
         let id_sketch = match rank_par {
             BoxType::Full(tol) => {
                 if *tol < num::One::one() {
+                    println!("Full");
                     far_field_sketch
                         .into_subview([0, 0], null_shape)
                         .into_id_alloc(
@@ -994,6 +995,7 @@ where
                         )
                         .unwrap()
                 } else {
+                    println!("Merged");
                     let loc_rank = null_shape[1].min(num::ToPrimitive::to_usize(tol).unwrap());
                     far_field_sketch
                         .into_subview([0, 0], null_shape)
